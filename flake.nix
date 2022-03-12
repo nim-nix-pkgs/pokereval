19dd35234909c68a654217072ed45a434d8726a9
hugosenari <hugosenari@gmail.com>
2022-03-12 04:02:33 -0300

    chore: re index pokereval

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''A poker hand evaluator'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."pokereval-master".url = "path:./master";
+  inputs."pokereval-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."pokereval-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."pokereval-v0_1_0".url = "path:./v0_1_0";
+  inputs."pokereval-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."pokereval-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A poker hand evaluator'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-pokereval-master.flake = false;
+  inputs.src-pokereval-master.owner = "jasonlu7";
+  inputs.src-pokereval-master.ref   = "refs/heads/master";
+  inputs.src-pokereval-master.repo  = "pokereval";
+  inputs.src-pokereval-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-pokereval-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-pokereval-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
